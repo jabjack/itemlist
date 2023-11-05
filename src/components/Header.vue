7e4bf52591832d67ceba1e7de58432f4b656955b
jabjack <jsperez9988@gmail.com>
2023-11-05 10:43:09 -0600

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+
+<template>
+  
+  <nav class=" w-full bg-teal-500 from-green-700 to-blue-600 text-white px-4 py-2  space-y-7 > * + *">
+
+       <a class= " font-serif content-center text-4xl items-center justify-center  subpixel-antialiased"> Item List </a> 
+       
+
+  </nav>
+
+
+
+
+</template>
+
+<script>
+export default {
+
+       // eslint-disable-next-line vue/multi-word-component-names
+       name: "Header",
+
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/components/Itemlist.vue
@@ -0,0 +1,94 @@
+<template>
+
+  <div class="m-auto space-y-6 > * + *" >
+
+
+
+    <h1 class=" font-serif text-2xl text-center mt-12 cursor-none truncate"> Fruit List  {{ herosCount }}</h1>
+
+    <ul>
+      <li
+        class="flex justify-between   mt-6 cursor-none"
+        v-for="(hero, index) in dcHeros"
+        :key="hero.name"
+
+      >
+        <div>
+          {{ hero.name }}
+        </div>
+
+        <button class="mx-20 opacity-3 bg-blend-normal" v-on:click="remove(index)"> x </button>
+
+      </li>
+ 
+
+    <form class="mt-10" @submit.prevent = "addHero">
+
+      <input v-model="newHero" class="border rounded space-x-1.5 > * + *  justify-between shadow-md " placeholder="Type the new Item" />
+
+      <button type="submit" class=" py-0 px-2 my-0 mx-50px bg-emerald-500 shadow-md   border rounded  mx-8 space-x-20 font-serif font-black tracking-tighter decoration-solid  text-white" > Add Item </button>
+
+
+    </form>
+
+  </ul>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: "Itemlist",
+
+  data() {
+
+    return {
+      newHero: "",
+
+      dcHeros: [
+        { name: "Coco" },
+        { name: "pineapple" },
+        { name: "apple" },
+        { name: "orange" },
+        { name: "lemon" },
+        { name: "strawberry" },
+      ],
+    };
+  },
+
+  computed: {
+
+    herosCount() {
+      return this.dcHeros.length;
+    },
+  },
+
+  methods: {
+
+    addHero() {
+
+      if (this.newHero != "") {
+
+        this.dcHeros.push({ name: this.newHero });
+
+        this.newHero = "";
+      }
+    },
+
+    remove(index) {
+
+      this.dcHeros = this.dcHeros.filter((hero, i) => i != index);
+      
+    },
+
+  },
+
+};
+</script>
+
+<style>
+</style>
